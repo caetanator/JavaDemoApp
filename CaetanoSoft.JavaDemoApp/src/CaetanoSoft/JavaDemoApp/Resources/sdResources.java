f2a2ba2309e104c35d8fc417a426ae04136000f3
José Caetano Silva <caetanator@users.noreply.github.com>
2026-03-31 06:15:19 +0100

    New version.

--- CaetanoSoft.JavaDemoApp/src/CaetanoSoft/JavaDemoApp/Resources/sdResources.java
+++ CaetanoSoft.JavaDemoApp/src/CaetanoSoft/JavaDemoApp/Resources/sdResources.java
@@ -1,31 +1,31 @@
 //******************************************************************************
-// Project: Splash Screen Manager Demo v1.00
-// URL:     http://slam.sourceforge.net
+// Project: CaetanoSoft.JavaDemoApp
+// URL:     https://github.com/caetanator/JavaJavaDemoApp/
 // File:    sdResources.java
 //
 // Description:
-//          Default and English language resources.
+//          Default and English language resources for CaetanoSoft.JavaDemoApp.
 //
-// Copyright (C) 2008:
-//          José Caetano Silva (jcaetano@users.sourceforge.net)
+// Copyright:
+//          © 2008-2026 José Caetano Silva / CaetanoSoft. All rights reserved.
 //
 // License:
-//          This file is part of Splash Screen Manager Demo.
+//          This file is part of CaetanoSoft.JavaDemoApp.
 //
-//          Splash Screen Manager Demo is free software: you can redistribute it and/or modify
-//          it under the terms of the GNU General Public License as published by
-//          the Free Software Foundation, either version 3 of the License, or
-//          (at your option) any later version.
+//          CaetanoSoft.JavaDemoApp is free software: you can redistribute it and/or 
+//          modify it under the terms of the GNU General Public License as 
+//          published by the Free Software Foundation, either version 3 of the 
+//          License, or (at your option) any later version.
 //
-//          Splash Screen Manager Demo is distributed in the hope that it will be useful,
-//          but WITHOUT ANY WARRANTY; without even the implied warranty of
-//          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//          CaetanoSoft.JavaDemoApp is distributed in the hope that it will be 
+//          useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+//          of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //          GNU General Public License for more details.
 //
 //          You should have received a copy of the GNU General Public License
-//          along with Splash Screen Manager Demo.  If not, see <http://www.gnu.org/licenses/>.
+//          along with CaetanoSoft.JavaDemoApp. If not, see 
+//          <https://www.gnu.org/licenses/gpl-3.0.html>.
 //******************************************************************************
-
 
 
 package CaetanoSoft.JavaDemoApp.Resources;
@@ -38,56 +38,56 @@ import javax.swing.*;
  * International resources for 'SplashScreenDemo.class'.<br>
  * <b>Default and English language.</b>
  *
- * @author      José Caetano Silva (jcaetano@users.sourceforge.net)
- * @version     1.00, 2008-06-10
+ * @author      José Caetano Silva
+ * @version     1.02.0001, 2008-06-10
  * @since       1.00
  */
 public class sdResources extends ListResourceBundle {
     static final Object[][] contents = {
         // LOCALIZE THIS
         { "MenuFile_T", "File" },
-        { "MenuFile_MN", new Integer(KeyEvent.VK_F) },
+        { "MenuFile_MN", KeyEvent.VK_F },
         
         { "MenuFileOpen_T", "Open..." },
         { "MenuFileOpen_TT", "Opens a file" }, 
-        { "MenuFileOpen_MN", new Integer(KeyEvent.VK_O) },
+        { "MenuFileOpen_MN", KeyEvent.VK_O },
         // {"MenuFileOpen_AK", null},
         
         { "MenuFileSave_T", "Save..." }, 
         { "MenuFileSave_TT", "Saves a file" },
-        { "MenuFileSave_MN", new Integer(KeyEvent.VK_S) },
+        { "MenuFileSave_MN", KeyEvent.VK_S },
         // {"MenuFileSave_AK", null},
         
         { "MenuFilePrint_T", "Print..." }, 
         { "MenuFilePrint_TT", "Prints a file" },
-        { "MenuFilePrint_MN", new Integer(KeyEvent.VK_P) },
+        { "MenuFilePrint_MN", KeyEvent.VK_P },
         { "MenuFilePrint_AK", KeyStroke.getKeyStroke(KeyEvent.VK_P, ActionEvent.CTRL_MASK).toString() },
         
         { "MenuFileExit_T", "Exit" }, 
         { "MenuFileExit_TT", "Exits the application" },
-        { "MenuFileExit_MN", new Integer(KeyEvent.VK_X) },
+        { "MenuFileExit_MN", KeyEvent.VK_X },
         // {"MenuFileExit_AK", null},
         
         
         { "MenuEdit_T", "Edit" },
-        { "MenuEdit_MN", new Integer(KeyEvent.VK_E) }, 
+        { "MenuEdit_MN", KeyEvent.VK_E }, 
         
         { "MenuEditColor_T", "Background Color..." }, 
         { "MenuEditColor_TT", "Choose the text background color" },
-        { "MenuEditColor_MN", new Integer(0) },
+        { "MenuEditColor_MN", 0 },
         // {"MenuEditColor_AK", null},
         
         { "MenuHelp_T", "Help" },
-        { "MenuHelp_MN", new Integer(KeyEvent.VK_H) }, 
+        { "MenuHelp_MN", KeyEvent.VK_H }, 
         
         { "MenuHelpLicense_T", "License..." }, 
         { "MenuHelpLicense_TT", "Shows the license" },
-        { "MenuHelpLicense_MN", new Integer(KeyEvent.VK_L) },
+        { "MenuHelpLicense_MN", KeyEvent.VK_L },
 
         // {"MenuHelpLicense_AK", null},
         { "MenuHelpAbout_T", "About..." }, 
         { "MenuHelpAbout_TT", "About this application" },
-        { "MenuHelpAbout_MN", new Integer(KeyEvent.VK_A) },
+        { "MenuHelpAbout_MN", KeyEvent.VK_A },
         { "MenuHelpAbout_AK", KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.ALT_MASK | ActionEvent.CTRL_MASK).toString() },
         
         { "MBCheckExit_T", "Do you real want to exit this application?" },
--- CaetanoSoft.JavaDemoApp/src/CaetanoSoft/JavaDemoApp/Resources/sdResources_pt.java
+++ CaetanoSoft.JavaDemoApp/src/CaetanoSoft/JavaDemoApp/Resources/sdResources_pt.java
@@ -1,31 +1,31 @@
 //******************************************************************************
-// Project: Splash Screen Manager Demo v1.00
-// URL:     http://slam.sourceforge.net
+// Project: CaetanoSoft.JavaDemoApp
+// URL:     https://github.com/caetanator/JavaJavaDemoApp/
 // File:    sdResources_pt.java
 //
 // Description:
-//          Portuguese language resources.
+//          Portuguese language resources for CaetanoSoft.JavaDemoApp.
 //
-// Copyright (C) 2008:
-//          José Caetano Silva (jcaetano@users.sourceforge.net)
+// Copyright:
+//          © 2008-2026 José Caetano Silva / CaetanoSoft. All rights reserved.
 //
 // License:
-//          This file is part of Splash Screen Manager Demo.
+//          This file is part of CaetanoSoft.JavaDemoApp.
 //
-//          Splash Screen Manager Demo is free software: you can redistribute it and/or modify
-//          it under the terms of the GNU General Public License as published by
-//          the Free Software Foundation, either version 3 of the License, or
-//          (at your option) any later version.
+//          CaetanoSoft.JavaDemoApp is free software: you can redistribute it and/or 
+//          modify it under the terms of the GNU General Public License as 
+//          published by the Free Software Foundation, either version 3 of the 
+//          License, or (at your option) any later version.
 //
-//          Splash Screen Manager Demo is distributed in the hope that it will be useful,
-//          but WITHOUT ANY WARRANTY; without even the implied warranty of
-//          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//          CaetanoSoft.JavaDemoApp is distributed in the hope that it will be 
+//          useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+//          of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //          GNU General Public License for more details.
 //
 //          You should have received a copy of the GNU General Public License
-//          along with Splash Screen Manager Demo.  If not, see <http://www.gnu.org/licenses/>.
+//          along with CaetanoSoft.JavaDemoApp. If not, see 
+//          <https://www.gnu.org/licenses/gpl-3.0.html>.
 //******************************************************************************
-
 
 
 package CaetanoSoft.JavaDemoApp.Resources;
@@ -38,55 +38,55 @@ import javax.swing.*;
  * International resources for 'SplashScreenDemo.class'.<br>
  * <b>Portuguese language.</b>
  *
- * @author      José Caetano Silva (jcaetano@users.sourceforge.net)
- * @version     1.00, 2008-06-10
+ * @author      José Caetano Silva
+ * @version     1.02.0001, 2008-06-10
  * @since       1.00
  */
 public class sdResources_pt extends ListResourceBundle {
     static final Object[][] contents = {
         // LOCALIZE THIS
         { "MenuFile_T", "Ficheiro" },
-        { "MenuFile_MN", new Integer(KeyEvent.VK_F) },
+        { "MenuFile_MN", KeyEvent.VK_F },
         
         { "MenuFileOpen_T", "Abrir..." },
         { "MenuFileOpen_TT", "Abre um ficheiro" }, 
-        { "MenuFileOpen_MN", new Integer(KeyEvent.VK_A) },
+        { "MenuFileOpen_MN", KeyEvent.VK_A },
         // {"MenuFileOpen_AK", null},
         
         { "MenuFileSave_T", "Guardar..." }, 
         { "MenuFileSave_TT", "Guarda um ficheiro" },
-        { "MenuFileSave_MN", new Integer(KeyEvent.VK_G) },
+        { "MenuFileSave_MN", KeyEvent.VK_G },
         // {"MenuFileSave_AK", null},
 
         { "MenuFilePrint_T", "Imprimir..." }, 
         { "MenuFilePrint_TT", "Imprime um ficheiro" },
-        { "MenuFilePrint_MN", new Integer(KeyEvent.VK_I) },
+        { "MenuFilePrint_MN", KeyEvent.VK_I },
         // {"MenuFilePrint_AK", null},
         
         { "MenuFileExit_T", "Sair" },
         { "MenuFileExit_TT", "Sai da aplicação" },
-        { "MenuFileExit_MN", new Integer(KeyEvent.VK_A) },
+        { "MenuFileExit_MN", KeyEvent.VK_A },
         { "MenuFileExit_AK", KeyStroke.getKeyStroke(KeyEvent.VK_P, ActionEvent.CTRL_MASK).toString() },
         
         { "MenuEdit_T", "Editar" },
-        { "MenuEdit_MN", new Integer(KeyEvent.VK_E) }, 
+        { "MenuEdit_MN", KeyEvent.VK_E }, 
         
         { "MenuEditColor_T", "Cor do Fundo..." }, 
         { "MenuEditColor_TT", "Escolhe a cor do fundo do texto" },
-        { "MenuEditColor_MN", new Integer(0) },
+        { "MenuEditColor_MN", 0 },
         // {"MenuEditColor_AK", null},
         
         { "MenuHelp_T", "Ajuda" },
-        { "MenuHelp_MN", new Integer(KeyEvent.VK_A) },        
+        { "MenuHelp_MN", KeyEvent.VK_A },        
        
         { "MenuHelpLicense_T", "Licença..." }, 
         { "MenuHelpLicense_TT", "Mostra a licença" },
-        { "MenuHelpLicense_MN", new Integer(KeyEvent.VK_L) },
+        { "MenuHelpLicense_MN", KeyEvent.VK_L },
         // {"MenuHelpLicense_AK", null},
 
         { "MenuHelpAbout_T", "Acerca..." }, 
         { "MenuHelpAbout_TT", "Acerca desta aplicação" },
-        { "MenuHelpAbout_MN", new Integer(KeyEvent.VK_A) },
+        { "MenuHelpAbout_MN", KeyEvent.VK_A },
         { "MenuHelpAbout_AK", KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.ALT_MASK | ActionEvent.CTRL_MASK).toString() },
  
         { "MBCheckExit_T", "Quer mesmo sair da aplicação?" },
